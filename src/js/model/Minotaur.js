690f05106810ac04ce9ce0f99d64f45d25dfc934
meatyminx <flexayoung@gmail.com>
2018-04-09 11:45:51 +0200

    mend

--- src/js/model/Minotaur.js
+++ src/js/model/Minotaur.js
@@ -32,51 +32,39 @@ class Minotaur extends GameItem {
   }
 
   calculatePath(userPos) {      
-    this.clearPathGrid();
-    this.path = [];
-     
-    this.traversed = [];
-
-    this.traverseTree(userPos);
-    this.setPath(this.traversed);
-  }    
-
-  traverseTree(userPos) {
+    console.log("user", userPos);
     const x = this.cell[1];
     const z = this.cell[0];
-    console.log('user', userPos);
-    console.log('minotaur', z, x);
-    
-    this.pathGrid[z][x] = true;
+    console.log("mino", z, x);
 
+    this.clearPathGrid();
+    this.path = [];
     this.stack = [];
-    this.stack.push([[null], [z, x]]);   
+    this.stack.push([[null], [z, x]]);        
+    this.pathGrid[z][x] = true;
     
-    while (this.stack.length !== 0) {
-      let prevStack = this.stack.pop();
-      let v = prevStack[1];
+    
+    while (this.stack.length !== 0) {     
+        let prevStack = this.stack.pop();
+        let v = prevStack[1];
+        
+        
+        // console.log(v.length, "vlength");
+        
+        if(v[0] !== null) {          
+          this.path.push(prevStack);
+          if (userPos[0] === v[0] && userPos[1] === v[1]) {
+            console.log("Found");
 
-      if (v[0] !== null) {
-        this.traversed.push([v[0], v[1]]);
-        if (userPos[0] === v[0] && userPos[1] === v[1]) {
-          return;
+            return;
+          } 
         }
-      }
-      if ((v[0] - 1) >= 0) this.mark(v, [v[0] - 1, v[1]], 'UP');
-      if ((v[0] + 1) < this.grid.length) this.mark(v, [v[0] + 1, v[1]], 'DOWN');
-      if ((v[1] - 1) >= 0) this.mark(v, [v[0], v[1] - 1], 'LEFT');
-      if ((v[1] + 1) < this.grid[0].length) this.mark(v, [v[0], v[1] + 1], 'RIGHT');
+        if((v[0]-1) >= 0) this.mark(v, [v[0]-1, v[1]], 'UP');
+        if((v[0]+1) < this.grid.length) this.mark(v, [v[0]+1, v[1]], 'DOWN');
+        if ((v[1] - 1) >= 0) this.mark(v, [v[0], v[1] - 1], 'LEFT');        
+        if ((v[1] + 1) < this.grid[0].length) this.mark(v, [v[0], v[1] + 1], 'RIGHT');
     }    
-  }
-
-  setPath() {
-    let currentNode = this.traversed[0]
-    console.log(this.traversed);
-    
-    // for(let i = this.traversed.length-1; i >= 0; i--) {
-    //   this.path.push()
-    // }
-  }
+  }    
 
   mark(currentTile, nextTile, direction) {    
     const z = currentTile[0];
